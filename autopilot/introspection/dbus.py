06a28fbda5c27d2b7066f4eba84b69e12be64019
Thomi Richards <thomi.richards@canonical.com>
2012-06-28 14:47:36 +1200

    Unity introspection is now dbus introspection.

--- autopilot/introspection/dbus.py
+++ autopilot/introspection/dbus.py
@@ -7,6 +7,13 @@
 # by the Free Software Foundation.
 #
 
+"""This module contains the code to retrieve state via DBus calls.
+
+Under normal circumstances, the only thing you should need to use from this module
+is the DBusIntrospectableObject class.
+
+"""
+
 from __future__ import absolute_import
 
 from dbus import Interface
@@ -39,85 +46,21 @@ class IntrospectableObjectMetaclass(type):
         return class_object
 
 
-# acquire the debugging dbus object
-UNITY_BUS_NAME = 'com.canonical.Unity'
-DEBUG_PATH = '/com/canonical/Unity/Debug'
-INTROSPECTION_IFACE = 'com.canonical.Unity.Debug.Introspection'
+INTROSPECTION_IFACE = 'com.canonical.Autopilot.Introspection'
 
 
-_debug_proxy_obj = session_bus.get_object(UNITY_BUS_NAME, DEBUG_PATH)
-_introspection_iface = Interface(_debug_proxy_obj, INTROSPECTION_IFACE)
-
-
-def get_state_by_path(piece='/Unity'):
-    """Returns a full dump of unity's state."""
-    return _introspection_iface.GetState(piece)
-
-
-def get_state_by_name_and_id(class_name, unique_id):
-    """Get a state dictionary from unity given a class name and id.
-
-    raises StateNotFoundError if the state is not found.
-
-    Returns a dictionary of information. Unlike get_state_by_path, this
-    method can never return state for more than one object.
-    """
-    try:
-        query = "//%(class_name)s[id=%(unique_id)d]" % (dict(
-                                                    class_name=class_name,
-                                                    unique_id=unique_id))
-        return get_state_by_path(query)[0]
-    except IndexError:
-        raise StateNotFoundError(class_name, unique_id)
-
-
-def make_introspection_object(dbus_tuple):
-    """Make an introspection object given a DBus tuple of (name, state_dict).
-
-    This only works for classes that derive from UnityIntrospectionObject.
-    """
-    name, state = dbus_tuple
-    try:
-        class_type = _object_registry[name]
-    except KeyError:
-        print name, "is not a valid introspection type!"
-        return None
-    return class_type(state)
-
-
-def start_log_to_file(file_path):
-    """Instruct Unity to start logging to the given file."""
-    _introspection_iface.StartLogToFile(file_path)
-
-
-def reset_logging():
-    """Instruct Unity to stop logging to a file."""
-    _introspection_iface.ResetLogging()
-
-
-def set_log_severity(component, severity):
-    """Instruct Unity to set a log component's severity.
-
-    'component' is the unity logging component name.
-
-    'severity' is the severity name (like 'DEBUG', 'INFO' etc.)
+def get_introspection_iface(service_name, object_path):
+    """Get the autopilot introspection interface for the specified service name
+    and object path.
 
     """
-    _introspection_iface.SetLogSeverity(component, severity)
+    if not isinstance(service_name, basestring):
+        raise TypeError("Service name must be a string.")
+    if not isinstance(object_path, basestring):
+        raise TypeError("Object name must be a string")
 
-
-def log_unity_message(severity, message):
-    """Instruct unity to log a message for us.
-
-    severity: one of ('TRACE', 'DEBUG', 'INFO', 'WARNING', 'ERROR').
-
-    message: The message to log.
-
-    For debugging purposes only! If you want to log a message during an autopilot
-    test, use the python logging framework instead.
-
-    """
-    _introspection_iface.LogMessage(severity, message)
+    _debug_proxy_obj = session_bus.get_object(service_name, object_path)
+    return Interface(_debug_proxy_obj, INTROSPECTION_IFACE)
 
 
 def translate_state_keys(state_dict):
@@ -125,9 +68,33 @@ def translate_state_keys(state_dict):
     return {k.replace('-','_'):v for k,v in state_dict.iteritems() }
 
 
-class UnityIntrospectionObject(object):
-    """A class that can be created using a dictionary of state from Unity."""
+def make_introspection_object(dbus_tuple):
+    """Make an introspection object given a DBus tuple of (name, state_dict).
+
+    This only works for classes that derive from DBusIntrospectionObject.
+    """
+    name, state = dbus_tuple
+    try:
+        class_type = _object_registry[name]
+    except KeyError:
+        logger.error("%s is not a valid introspection type!", name)
+        return None
+    return class_type(state)
+
+
+class DBusIntrospectionObject(object):
+    """A class that can be created using a dictionary of state from DBus.
+
+    To use this class properly you must set the DBUS_SERVICE and DBUS_OBJECT
+    class attributes. THey should be set to the Service name and object path
+    where the autopilot interface is being exposed.
+
+    """
+
     __metaclass__ = IntrospectableObjectMetaclass
+
+    DBUS_SERVICE = None
+    DBUS_OBJECT = None
 
     def __init__(self, state_dict):
         self.__state = {}
@@ -175,10 +142,12 @@ class UnityIntrospectionObject(object):
                 expected_value = Equals(expected_value)
 
             for i in range(10):
-                new_state = translate_state_keys(get_state_by_name_and_id(
-                                                self.parent.__class__.__name__,
-                                                self.parent.id)
-                                                )
+                new_state = translate_state_keys(
+                                self.parent.get_state_by_name_and_id(
+                                        self.parent.__class__.__name__,
+                                        self.parent.id
+                                    )
+                                )
                 new_value = new_state[self.name]
                 # Support for testtools.matcher classes:
                 mismatch = expected_value.match(new_value)
@@ -212,12 +181,12 @@ class UnityIntrospectionObject(object):
     def _get_child_tuples_by_type(self, desired_type):
         """Get a list of (name,dict) pairs from children of the specified type.
 
-        desired_type must be a subclass of UnityIntrospectionObject.
+        desired_type must be a subclass of DBusIntrospectionObject.
 
         """
-        if not issubclass(desired_type, UnityIntrospectionObject):
+        if not issubclass(desired_type, DBusIntrospectionObject):
             raise TypeError("%r must be a subclass of %r" % (desired_type,
-                UnityIntrospectionObject))
+                DBusIntrospectionObject))
 
         children = getattr(self, 'Children', [])
         results = []
@@ -234,7 +203,7 @@ class UnityIntrospectionObject(object):
     def get_children_by_type(self, desired_type, **kwargs):
         """Get a list of children of the specified type.
 
-        desired_type must be a subclass of UnityIntrospectionObject.
+        desired_type must be a subclass of DBusIntrospectionObject.
 
         Keyword arguments can be used to restrict returned instances. For example:
 
@@ -266,7 +235,7 @@ class UnityIntrospectionObject(object):
 
         """
         # need to get name from class object.
-        new_state = get_state_by_name_and_id(self.__class__.__name__, self.id)
+        new_state = self.get_state_by_name_and_id(self.__class__.__name__, self.id)
         self.set_properties(new_state)
 
     @classmethod
@@ -281,7 +250,7 @@ class UnityIntrospectionObject(object):
 
         """
         cls_name = cls.__name__
-        instances = get_state_by_path("//%s" % (cls_name))
+        instances = cls.get_state_by_path("//%s" % (cls_name))
         return [make_introspection_object((cls_name,i)) for i in instances]
 
     def __getattr__(self, name):
@@ -295,3 +264,32 @@ class UnityIntrospectionObject(object):
             return self.__state[name]
         # attribute not found.
         raise AttributeError("Attribute '%s' not found." % (name))
+
+    @classmethod
+    def get_state_by_path(cls, piece='/Unity'):
+        """Returns a full dump of unity's state."""
+        return get_introspection_iface(
+            cls.DBUS_SERVICE,
+            cls.DBUS_OBJECT
+            ).GetState(piece)
+
+    def get_state_by_name_and_id(self, class_name, unique_id):
+        """Get a state dictionary from unity given a class name and id.
+
+        raises StateNotFoundError if the state is not found.
+
+        Returns a dictionary of information. Unlike get_state_by_path, this
+        method can never return state for more than one object.
+        """
+        try:
+            query = "//%(class_name)s[id=%(unique_id)d]" % (dict(
+                                                        class_name=class_name,
+                                                        unique_id=unique_id))
+            return self.get_state_by_path(query)[0]
+        except IndexError:
+            raise StateNotFoundError(class_name, unique_id)
+
+
+
+
+
