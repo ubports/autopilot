47e31a3b4acbd3cf592ed364147ec9cf59196a1b
Brandon Schaefer <brandontschaefer@gmail.com>
2012-06-27 17:09:26 -0700

    * Adds zeitgeist support to autopilot so we can add file to the file lens

---
+++ autopilot/emulators/zeitgeists.py
@@ -0,0 +1,67 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+# Copyright 2012 Canonical
+# Author: Brandon Schaefer
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+
+"""Class to add text files to the file lens."""
+
+from __future__ import absolute_import
+
+from zeitgeist.datamodel import Event, Subject, Interpretation, Manifestation, ResultType
+from zeitgeist.client import ZeitgeistClient
+import logging
+import tempfile
+import os.path
+
+class Zeitgesit(object):
+    """Class to access zeitgesit."""
+
+    def __init__(self):
+        self.zg = ZeitgeistClient()
+        self.temp_file = None
+        self.logger = logging.getlogger(__name__)
+
+    def add_existing_file(self, path):
+        """Takes a complete path to an existing text file add it to the file lens."""
+        if os.path.exists(path):
+            self.__add_text_file(path)
+        else:
+          self.logger.info("File not found on path: %s." %(path))
+
+    def add_temp_file(self):
+        """Creates a named temporary file then adds it to the file lens."""
+        self.temp_file = tempfile.NamedTemporaryFile()
+        self.__add_text_file(self.temp_file.name)
+
+    def __add_text_file(self, path):
+        """Takes a path to a file and creates an event for it then querys it."""
+        file_lens = "file://"
+        dir_path = os.path.dirname(path)
+        name = os.path.basename(path)
+
+        event = Event.new_for_values (interpretation=Interpretation.ACCESS_EVENT,
+                                      manifestation=Manifestation.USER_ACTIVITY,
+                                      subject_uri=file_lens + path,
+                                      subject_interpretation=Interpretation.TEXT_DOCUMENT,
+                                      subject_manifestation=Manifestation.FILE_DATA_OBJECT,
+                                      subject_origin=file_lens + dir_path,
+                                      subject_text=name)
+        self.zg.insert_event(event)
+
+        template = Event.new_for_values (interpretation=Interpretation.ACCESS_EVENT,
+                                         manifestation=Manifestation.USER_ACTIVITY)
+
+        self.zg.find_events_for_template (template,
+                                     self.__log_events_cb,
+                                     num_events=1,
+                                     result_type=ResultType.MostRecentSubjects)
+
+    def __log_events_cb(self, events):
+        """Callback to recive events, we are just using it to log each event."""
+        self.logger.info("Found Events")
+        for event in events:
+            for subject in event.subjects:
+                self.logger.info(" * %s" % (subject.uri))
