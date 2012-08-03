7d5431c46d6def50bd2faacbc0f7f6eb822423d9
Thomi Richards <thomi.richards@canonical.com>
2012-08-03 09:48:32 -0400

    Code cleanup

--- autopilot/glibrunner.py
+++ autopilot/glibrunner.py
@@ -11,11 +11,7 @@ from __future__ import absolute_import
 import dbus.glib
 
 import sys
-# import dbus
-# from dbus.mainloop.glib import DBusGMainLoop
 import glib
-# import gtk
-# import gtk.gdk
 import testtools
 import time
 import threading
@@ -29,8 +25,6 @@ except:
 # If you don't, random glib/gobject/gtk functions will hang...
 glib.threads_init()
 dbus.glib.threads_init()
-# dbus.mainloop.glib.threads_init()
-# gtk.gdk.threads_init()
 
 class WorkerThread(threading.Thread):
     def __init__(self, loop, worker):
@@ -43,34 +37,24 @@ class WorkerThread(threading.Thread):
     def run(self):
         try:
             while self.loop.is_running() is False:
-                # _print("Thread '%i': mainloop not yet running, backing off" % (self.ident, ))
                 time.sleep(0.5)
                 continue
-            # from autopilot.emulators.bamf import Bamf
-            # print Bamf().get_running_applications()
             self.result = self.worker()
         except:
             self.errors.append(sys.exc_info())
             # XXX: Not sure if this is needed / desired
             raise
         finally:
-            # print "Quitting main loop now."
             glib.idle_add(self.loop.quit)
-            # gtk.mainquit()
-            print "Done"
 
 
 def run_in_glib_loop(function, *args, **kwargs):
-    # set_main_loop()
     loop = glib.MainLoop()
-    # dbus.set_default_main_loop(dbus_loop)
-
     thread = WorkerThread(loop, lambda: function(*args, **kwargs))
     thread.start()
     loop.run()
-    print "Loop finished."
     thread.join()
-    # print "Thread joined"
+
     if thread.is_alive():
         raise RuntimeError("Test %r did not exit after 120 seconds." % function)
 
