f1d411476f4fa321d641659ed7fa4fdcd9da9747
Thomi Richards <thomi.richards@canonical.com>
2012-08-21 11:57:20 +1200

    Added missing copyright headers.

--- autopilot/vis/bus_enumerator.py
+++ autopilot/vis/bus_enumerator.py
@@ -1,3 +1,10 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+# Copyright 2012 Canonical
+# Author: Thomi Richards
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
 from collections import defaultdict
 import dbus
 from dbus.mainloop.qt import DBusQtMainLoop
