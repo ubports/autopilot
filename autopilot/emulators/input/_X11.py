e044955a875aef923306426e49f2ed2d83809a1f
Thomi Richards <thomi.richards@canonical.com>
2013-02-28 14:11:18 +1300

    Cleanup copyright header.

--- autopilot/emulators/input/_X11.py
+++ autopilot/emulators/input/_X11.py
@@ -1,6 +1,6 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
-# Copyright 2010 Canonical
-# Author: Alex Launi
+# Copyright 2013 Canonical
+# Author: Thomi Richards
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3, as published
