90178ab4a576b55e43b2a6f3961c0ac336591b13
Thomi Richards <thomi.richards@canonical.com>
2013-04-22 16:02:48 +1200

    Skip uinput tests instead of failing, since there is a bug to fix the reason they're failing.

--- autopilot/tests/test_input_stack.py
+++ autopilot/tests/test_input_stack.py
@@ -6,7 +6,9 @@
 # under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
 
+import os
 from testtools.matchers import IsInstance
+from unittest import SkipTest
 
 from autopilot.testcase import AutopilotTestCase
 from autopilot.input import Keyboard
@@ -17,6 +19,12 @@ class InputStackKeyboardTests(AutopilotTestCase):
         ('X11', dict(backend='X11')),
         ('UInput', dict(backend='UInput')),
         ]
+
+    def setUp(self):
+        super(InputStackKeyboardTests, self).setUp()
+        if self.backend == 'UInput' and not os.access('/dev/uinput', os.W_OK):
+            raise SkipTest("UInput backend currently requires write access to /dev/uinput")
+
 
     def test_can_create_backend(self):
         keyboard = Keyboard.create(self.backend)
