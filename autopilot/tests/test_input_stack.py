b75b4757db74f6672ddfa29588a9c3381851fde3
Thomi Richards <thomi.richards@canonical.com>
2013-04-22 16:23:08 +1200

    Commit prior to merging in chris's fixes.

--- autopilot/tests/test_input_stack.py
+++ autopilot/tests/test_input_stack.py
@@ -7,11 +7,31 @@
 # by the Free Software Foundation.
 
 import os
+from tempfile import mktemp
 from testtools.matchers import IsInstance
+from textwrap import dedent
 from unittest import SkipTest
 
 from autopilot.testcase import AutopilotTestCase
 from autopilot.input import Keyboard
+
+simple_qml = dedent("""\
+    import QtQuick 2.0
+
+    Rectangle {
+        width: 800
+        height: 600
+
+        TextEdit {
+            id: text_edit1
+            objectName: test_text
+            anchors.fill: parent
+            text: qsTr("Text Edit")
+            font.pixelSize: 12
+        }
+    }
+    """)
+
 
 class InputStackKeyboardTests(AutopilotTestCase):
 
@@ -29,3 +49,13 @@ class InputStackKeyboardTests(AutopilotTestCase):
     def test_can_create_backend(self):
         keyboard = Keyboard.create(self.backend)
         self.assertThat(keyboard, IsInstance(Keyboard))
+
+    def start_qml_app(self, qml_string):
+        qml_file = mktemp(suffix='.qml')
+        with open(qml_file, 'w') as f:
+            f.write(qml_string)
+            self.addCleanup(os.remove, qml_file)
+        return self.launch_test_application('qmlscene', qml_file)
+
+    def test_some_text(self):
+        app_proxy = self.start_qml_app(simple_qml)
