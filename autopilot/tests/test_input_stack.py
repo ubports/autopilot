e570e4e96316ebe47d5afa6e83a71252d50288ac
Thomi Richards <thomi.richards@canonical.com>
2013-04-23 15:14:32 +1200

    Added new keyboard tests that make use of the window mocker, and removed the old crappy tests.

--- autopilot/tests/test_input_stack.py
+++ autopilot/tests/test_input_stack.py
@@ -6,34 +6,18 @@
 # under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
 
+import json
 import os
 from tempfile import mktemp
-from testtools.matchers import IsInstance
-from textwrap import dedent
+from testtools.matchers import IsInstance, Equals
 from unittest import SkipTest
 
-from autopilot.testcase import AutopilotTestCase
+from autopilot.testcase import AutopilotTestCase, multiply_scenarios
 from autopilot.input import Keyboard
-
-simple_qml = dedent("""\
-    import QtQuick 2.0
-
-    Rectangle {
-        width: 800
-        height: 600
-
-        TextEdit {
-            id: text_edit1
-            objectName: test_text
-            anchors.fill: parent
-            text: qsTr("Text Edit")
-            font.pixelSize: 12
-        }
-    }
-    """)
+from autopilot.matchers import Eventually
 
 
-class InputStackKeyboardTests(AutopilotTestCase):
+class InputStackKeyboardBase(AutopilotTestCase):
 
     scenarios = [
         ('X11', dict(backend='X11')),
@@ -41,21 +25,57 @@ class InputStackKeyboardTests(AutopilotTestCase):
         ]
 
     def setUp(self):
-        super(InputStackKeyboardTests, self).setUp()
+        super(InputStackKeyboardBase, self).setUp()
         if self.backend == 'UInput' and not os.access('/dev/uinput', os.W_OK):
             raise SkipTest("UInput backend currently requires write access to /dev/uinput")
+
+
+class InputStackKeyboardCreationTests(InputStackKeyboardBase):
 
 
     def test_can_create_backend(self):
         keyboard = Keyboard.create(self.backend)
         self.assertThat(keyboard, IsInstance(Keyboard))
 
-    def start_qml_app(self, qml_string):
-        qml_file = mktemp(suffix='.qml')
-        with open(qml_file, 'w') as f:
-            f.write(qml_string)
-            self.addCleanup(os.remove, qml_file)
-        return self.launch_test_application('/usr/bin/qmlscene', qml_file)
+
+class InputStackKeyboardTypingTests(InputStackKeyboardBase):
+
+    scenarios = multiply_scenarios(
+        InputStackKeyboardBase.scenarios,
+        [
+            ('lower_alpha', dict(input='abcdefghijklmnopqrstuvwxyz')),
+            ('upper_alpha', dict(input='ABCDEFGHIJKLMNOPQRSTUVWXYZ')),
+            ('numeric', dict(input='0123456789')),
+            ('punctuation', dict(input='`~!@#$%^&*()_-+={}[]|\\:;"\'<>,.?/'))
+        ]
+        )
+
+    def start_mock_app(self):
+        window_spec_file = mktemp(suffix='.json')
+        window_spec = { "Contents": "TextEdit" }
+        json.dump(
+            window_spec,
+            open(window_spec_file, 'w')
+            )
+
+        return self.launch_test_application('window-mocker', window_spec_file)
+
+    def pick_app_launcher(self, app_path):
+        # force Qt app introspection:
+        from autopilot.introspection.qt import QtApplicationLauncher
+        return QtApplicationLauncher()
 
     def test_some_text(self):
-        app_proxy = self.start_qml_app(simple_qml)
+        app_proxy = self.start_mock_app()
+        text_edit = app_proxy.select_single('QTextEdit')
+
+        # make sure the text edit has keyboard focus:
+        self.mouse.move_to_object(text_edit)
+        self.mouse.click()
+
+        # create keyboard and type the text.
+        keyboard = Keyboard.create(self.backend)
+        keyboard.type(self.input, 0.01)
+
+        self.assertThat(text_edit.plainText, Eventually(Equals(self.input)))
+
