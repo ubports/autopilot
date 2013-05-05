d276db865d1528b22ec02b8e607358b9f6dc9ec5
Thomi Richards <thomi.richards@canonical.com>
2013-05-06 09:22:50 +1200

    Moved test that relied on gedit into the functional section. Added python-mock as a build-dep, so we can run the unit tests.

--- autopilot/tests/functional/test_application_mixin.py
+++ autopilot/tests/functional/test_application_mixin.py
@@ -26,14 +26,18 @@ from testtools.matchers import raises
 
 class ApplicationSupportTests(AutopilotTestCase):
 
-    def test_launch_with_bad_types_raises_typeerror(self):
-        """Calling launch_test_application with something other than a string must
-        raise a TypeError"""
+    def test_launch_raises_ValueError_on_unknown_kwargs(self):
+        """launch_test_application must raise ValueError when given unknown
+        keyword arguments.
 
-        self.assertThat(lambda: self.launch_test_application(1), raises(TypeError))
-        self.assertThat(lambda: self.launch_test_application(True), raises(TypeError))
-        self.assertThat(lambda: self.launch_test_application(1.0), raises(TypeError))
-        self.assertThat(lambda: self.launch_test_application(object()), raises(TypeError))
-        self.assertThat(lambda: self.launch_test_application(None), raises(TypeError))
-        self.assertThat(lambda: self.launch_test_application([]), raises(TypeError))
-        self.assertThat(lambda: self.launch_test_application((None,)), raises(TypeError))
+        """
+        fn = lambda: self.launch_test_application('gedit', arg1=123, arg2='asd')
+        self.assertThat(fn, raises(ValueError("Unknown keyword arguments: 'arg1', 'arg2'.")))
+
+    def test_launch_raises_ValueError_on_unknown_kwargs_with_known(self):
+        """launch_test_application must raise ValueError when given unknown
+        keyword arguments.
+
+        """
+        fn = lambda: self.launch_test_application('gedit', arg1=123, arg2='asd', launch_dir='/')
+        self.assertThat(fn, raises(ValueError("Unknown keyword arguments: 'arg1', 'arg2'.")))
