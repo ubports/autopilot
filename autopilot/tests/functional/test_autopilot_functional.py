29d95cbbbf36cd0d8b65242108c6ef7ec1e3ae92
Thomi Richards <thomi.richards@canonical.com>
2013-09-25 10:42:25 +1200

    Fix failing test.

--- autopilot/tests/functional/test_autopilot_functional.py
+++ autopilot/tests/functional/test_autopilot_functional.py
@@ -120,24 +120,10 @@ Loading tests from: %s
             """)
         )
         code, output, error = self.run_autopilot_list()
-        expected_regex = '''\
-Loading tests from: %s
-
-Failed to import test module: tests.test_simple
-Traceback \(most recent call last\):
-  File "/usr/lib/python2.7/unittest/loader.py", line 252, in _find_tests
-    module = self._get_module_from_name\(name\)
-  File "/usr/lib/python2.7/unittest/loader.py", line 230, in \
-_get_module_from_name
-    __import__\(name\)
-  File "/tmp/\w*/tests/test_simple.py", line 4, in <module>
-    import asdjkhdfjgsdhfjhsd
-ImportError: No module named asdjkhdfjgsdhfjhsd
-
-''' % self.base_path
+        expected_output = '''ImportError: No module named asdjkhdfjgsdhfjhsd'''
         self.assertThat(code, Equals(0))
         self.assertThat(error, Equals(''))
-        self.assertTrue(re.search(expected_regex, output, re.MULTILINE))
+        self.assertThat(output, Contains(expected_output))
 
     def test_list_tests_with_syntax_error(self):
         self.create_test_file(
