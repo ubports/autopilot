a436993690d7737ef3616847ceb358e193487a90
Christopher Lee <chris.lee@canonical.com>
2013-09-26 17:11:15 +1200

    Made fall-through case clearer

--- autopilot/introspection/dbus.py
+++ autopilot/introspection/dbus.py
@@ -572,10 +572,12 @@ class DBusIntrospectionObject(object):
                 "generic class.", name)
             # we want the object to inherit from the custom emulator base, not
             # the object class that is doing the selecting
-            base_class = cls
             for base in cls.__bases__:
                 if issubclass(base, CustomEmulatorBase):
                     base_class = base
+                    break
+            else:
+                base_class = cls
             class_type = type(str(name), (base_class,), {})
         return class_type(state, path)
 
