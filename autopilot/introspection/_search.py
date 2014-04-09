1a73d5ee3eca355eb2891359723c95ae91825c34
Christopher Lee <chris.lee@canonical.com>
2014-04-09 12:06:17 +1200

    Removed unused code.

--- autopilot/introspection/_search.py
+++ autopilot/introspection/_search.py
@@ -22,98 +22,6 @@
 from autopilot.utilities import _raise_on_unknown_kwargs
 
 
-class FilterDecision(object):
-    """Makes the decision if this connection matches the criteria given the
-    parameters.
-
-    If the connection passes the criteria the 'success' callable is called
-    passed the provided details and it's result is returned (which may be
-    the return of a filter called by that filter and so on.)
-
-    By default a non-applicable result returns false.
-
-
-    !!! Is this getting too complicated? Should there just be a
-        'add_next_filter' method that gets called if required?
-
-    """
-    def _pass(dbus_connection, params):
-        return True, params
-
-    def _fail(dbs_connection, params):
-        return False, params
-
-    def __init__(
-            self,
-            on_success=_pass,
-            on_failure=_fail,
-            on_not_applicable=_fail
-    ):
-        # self._on_success = lambda params: (True, params)
-        # self._on_not_applicable = lambda params: (False, params)
-        # self._on_failure = lambda params: (False, params)
-        self._on_success = on_success
-        self._on_failure = on_failure
-        self._on_not_applicable = on_not_applicable
-
-    def __call__(self, dbus_connection, parameters):
-        """This is where the decision is actually made.
-
-        Returns a tuple containing the filter result and the passed parameters
-        that may have beeb modified in the process of the filter.
-        (for instance some cached values may have been added or flags set.)
-
-        for instance:
-
-        if passes_test(dbus_conn, parameters):
-            return self._on_success(dbus_conn, parameters)
-        else if not applicable(dbus_conn, parameters):
-            return self._on_not_applicable(dbus_conn, parameters)
-        else:
-            return self._on_failure(dbus_conn, parameters)
-        """
-        raise NotImplementedError("You cannot use this class directly.")
-
-    def add_on_success(self, filter):
-        self._raise_if_not_callable(filter)
-        self._on_success = filter
-
-    def add_on_not_applicable(self, filter):
-        self._raise_if_not_callable(filter)
-        self._on_not_applicable = filter
-
-    def add_on_failure(self, filter):
-        self._raise_if_not_callable(filter)
-        self._on_failure = filter
-
-    def _raise_if_not_callable(self, filter):
-        if not isinstance(filter, FilterDecision) and not callable(filter):
-            raise RuntimeError("Filter must be a callable method or object.")
-
-
-# class DbusConnection(object):
-#     """Encapsulates the connection details."""
-#     pass
-
-
-class SearchParamters(object):
-    """Encapsulates the search criteria details."""
-
-    def __init__(self, **kwargs):
-        self._parameters = dict(kwargs)
-
-    def get(self, arg):
-        try:
-            return self._parameters[arg]
-        except KeyError:
-            # raise SomeCustomExceptionPerhaps
-            pass
-
-    def __str__(self):
-        # duplicate _get_search_criteria_string_representation
-        pass
-
-
 class FilterResult(object):
     PASS = object()
     FAIL = object()
@@ -147,12 +55,10 @@ def FilterListGenerator(search_parameters, parameter_filter_lookup):
         for search_key in search_param_copy.keys():
             filter_list.append(parameter_filter_lookup[search_key])
             search_param_copy.pop(search_key)
-    except KeyError as e:
+    except KeyError:
         raise KeyError(
             "Search parameter %s doesn't have a corresponding filter in %r"
             % (search_key, parameter_filter_lookup),
         )
-
-    _raise_on_unknown_kwargs(search_param_copy)
 
     return filter_list
