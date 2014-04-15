aae8dd8b624241fad43bff3e36da23fca8a7ffba
Thomi Richards <thomi.richards@canonical.com>
2014-04-15 13:11:33 +1200

    Fix eventually tests.

--- autopilot/tests/unit/test_matchers.py
+++ autopilot/tests/unit/test_matchers.py
@@ -50,7 +50,7 @@ def make_fake_attribute_with_result(result, attribute_type='wait_for',
     """
     class FakeObject(DBusIntrospectionObject):
         def __init__(self, props):
-            super(FakeObject, self).__init__(props, "/FakeObject", None)
+            super(FakeObject, self).__init__(props.copy(), "/FakeObject", None)
             FakeObject._fake_props = props
 
         @classmethod
