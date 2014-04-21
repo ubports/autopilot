62f1b935753b32ffd346174fd6b2d6426cc95043
Thomi Richards <thomi.richards@canonical.com>
2014-04-22 10:42:03 +1200

    Add some tests to cover the backends module.

--- autopilot/tests/unit/test_introspection_backends.py
+++ autopilot/tests/unit/test_introspection_backends.py
@@ -17,45 +17,116 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-
+from mock import patch
 from testtools import TestCase
 from testtools.matchers import Equals, Not, NotEquals
-from autopilot.introspection.backends import DBusAddress
+
+from autopilot.introspection import backends
 
 
 class DBusAddressTests(TestCase):
 
     def test_can_construct(self):
         fake_bus = object()
-        DBusAddress(fake_bus, "conn", "path")
+        backends.DBusAddress(fake_bus, "conn", "path")
 
     def test_can_store_address_in_dictionary(self):
         fake_bus = object()
-        DBusAddress(fake_bus, "conn", "path")
+        backends.DBusAddress(fake_bus, "conn", "path")
         dict(addr=object())
 
     def test_equality_operator(self):
         fake_bus = object()
-        addr1 = DBusAddress(fake_bus, "conn", "path")
+        addr1 = backends.DBusAddress(fake_bus, "conn", "path")
 
         self.assertThat(
-            addr1, Equals(DBusAddress(fake_bus, "conn", "path")))
+            addr1,
+            Equals(backends.DBusAddress(fake_bus, "conn", "path"))
+        )
         self.assertThat(
-            addr1, Not(Equals(DBusAddress(fake_bus, "conn", "new_path"))))
+            addr1,
+            NotEquals(backends.DBusAddress(fake_bus, "conn", "new_path"))
+        )
         self.assertThat(
-            addr1, Not(Equals(DBusAddress(fake_bus, "conn2", "path"))))
+            addr1,
+            NotEquals(backends.DBusAddress(fake_bus, "conn2", "path"))
+        )
         self.assertThat(
-            addr1, Not(Equals(DBusAddress(object(), "conn", "path"))))
+            addr1,
+            NotEquals(backends.DBusAddress(object(), "conn", "path"))
+        )
 
     def test_inequality_operator(self):
         fake_bus = object()
-        addr1 = DBusAddress(fake_bus, "conn", "path")
+        addr1 = backends.DBusAddress(fake_bus, "conn", "path")
 
         self.assertThat(
-            addr1, Not(NotEquals(DBusAddress(fake_bus, "conn", "path"))))
+            addr1,
+            Not(NotEquals(backends.DBusAddress(fake_bus, "conn", "path")))
+        )
         self.assertThat(
-            addr1, NotEquals(DBusAddress(fake_bus, "conn", "new_path")))
+            addr1,
+            NotEquals(backends.DBusAddress(fake_bus, "conn", "new_path"))
+        )
         self.assertThat(
-            addr1, NotEquals(DBusAddress(fake_bus, "conn2", "path")))
+            addr1,
+            NotEquals(backends.DBusAddress(fake_bus, "conn2", "path"))
+        )
         self.assertThat(
-            addr1, NotEquals(DBusAddress(object(), "conn", "path")))
+            addr1,
+            NotEquals(backends.DBusAddress(object(), "conn", "path"))
+        )
+
+    def test_session_bus_construction(self):
+        connection = self.getUniqueString()
+        object_path = self.getUniqueString()
+        with patch.object(backends, 'get_session_bus') as patch_sb:
+            addr = backends.DBusAddress.SessionBus(connection, object_path)
+            self.assertThat(
+                addr._addr_tuple,
+                Equals(
+                    backends.DBusAddress.AddrTuple(
+                        patch_sb.return_value,
+                        connection,
+                        object_path
+                    )
+                )
+            )
+
+    def test_system_bus_construction(self):
+        connection = self.getUniqueString()
+        object_path = self.getUniqueString()
+        with patch.object(backends, 'get_system_bus') as patch_sb:
+            addr = backends.DBusAddress.SystemBus(connection, object_path)
+            self.assertThat(
+                addr._addr_tuple,
+                Equals(
+                    backends.DBusAddress.AddrTuple(
+                        patch_sb.return_value,
+                        connection,
+                        object_path
+                    )
+                )
+            )
+
+    def test_custom_bus_construction(self):
+        connection = self.getUniqueString()
+        object_path = self.getUniqueString()
+        bus_path = self.getUniqueString()
+        with patch.object(backends, 'get_custom_bus') as patch_cb:
+            addr = backends.DBusAddress.CustomBus(
+                bus_path,
+                connection,
+                object_path
+            )
+            self.assertThat(
+                addr._addr_tuple,
+                Equals(
+                    backends.DBusAddress.AddrTuple(
+                        patch_cb.return_value,
+                        connection,
+                        object_path
+                    )
+                )
+            )
+            patch_cb.assert_called_once_with(bus_path)
