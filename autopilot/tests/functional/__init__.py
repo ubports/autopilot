b817c753ed01f649ccd4f2b88cd71c6b2385c23a
Thomi Richards <thomi.richards@canonical.com>
2014-04-30 09:41:56 +1200

    Fix failing tests.

--- autopilot/tests/functional/__init__.py
+++ autopilot/tests/functional/__init__.py
@@ -172,12 +172,12 @@ def _get_environment_patch(pythonpath):
 
 load_entry_point_script = """\
 #!/usr/bin/python
-__requires__ = 'autopilot==1.4.0'
+__requires__ = 'autopilot==1.5.0'
 import sys
 from pkg_resources import load_entry_point
 
 if __name__ == '__main__':
     sys.exit(
-        load_entry_point('autopilot==1.4.0', 'console_scripts', 'autopilot')()
+        load_entry_point('autopilot==1.5.0', 'console_scripts', 'autopilot')()
     )
 """
