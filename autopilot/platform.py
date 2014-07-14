a970585ce041ff77ff57b9777927435f71f7212a
Christopher Lee <chris.lee@canonical.com>
2014-07-14 20:05:55 +1200

    Printing environment for testing

--- autopilot/platform.py
+++ autopilot/platform.py
@@ -162,11 +162,11 @@ def get_display_server():
 def _display_is_x11():
     _logger.warning("Checking if display is x11")
     try:
+        print("Output: {}".format(os.environ))
         os.environ['DISPLAY']
-        _logger.warning("Output: {}".format(os.environ))
         return True
     except KeyError as e:
-        _logger.warning("Exception for x11: {}".format(e))
+        print("Exception for x11: {}".format(e))
         return False
 
 
