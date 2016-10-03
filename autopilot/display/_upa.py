bdcb2d4e0daa45a285f631612da7a20b7a5f25fc
Omer Akram <omer.akram@canonical.com>
2016-10-03 14:39:49 +0500

    Implement preliminary dynamic resolution grabbing logic, still need improvements

--- autopilot/display/_upa.py
+++ autopilot/display/_upa.py
@@ -17,17 +17,109 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import os
+import subprocess
 
 from autopilot.display import Display as DisplayBase
-from autopilot.platform import image_codename
-import subprocess
+from autopilot.introspection.utilities import process_util
+
+DISPLAY_SERVER_X = 'XOrg'
+DISPLAY_SERVER_MIR = 'Mir'
+ENV_XDG_RUNTIME_DIR = 'XDG_RUNTIME_DIR'
+PROCESS_NAME_X_SERVER = 'Xorg'
+SOCKET_MIR = 'mir_socket'
 
 
 def query_resolution():
+    display_server = query_current_display_server()
+    if display_server == DISPLAY_SERVER_X:
+        return _get_resolution_from_xrandr()
+    elif display_server == DISPLAY_SERVER_MIR:
+        return _get_resolution_from_mirout()
+    else:
+        raise RuntimeError(
+            'Unknown display server. Only {} and {} are supported.'.format(
+                DISPLAY_SERVER_MIR,
+                DISPLAY_SERVER_X
+            )
+        )
+
+
+def _get_resolution_from_xrandr():
+    xrandr_stdout = subprocess.check_output(
+        ['xrandr', '--current'],
+        universal_newlines=True
+    ).split('\n')
+    for line in xrandr_stdout:
+        if '*' in line:
+            return _grab_resolution_from_line(line)
+    raise ValueError('Something for now')
+
+
+def _get_process_environ_path(process_name):
+    return '/proc/{}/environ'.format(
+        process_util.get_pid_for_process(process_name)
+    )
+
+
+def _get_process_environ(process_name):
+    with open(_get_process_environ_path(process_name), 'r') as file:
+        output = file.read()
+    return output.split('\0')
+
+
+def _get_environ_variable_for_process(variable, process_name):
+    all_variables = _get_process_environ(process_name)
+    for var in all_variables:
+        if var and var.startswith(variable) and var.index('=') == len(variable):
+            return var.split('=')[1]
+
+
+def _get_unity8_mir_socket():
+    return _get_environ_variable_for_process('UNITY_MIR_SOCKET', 'unity8')
+
+
+def _grab_resolution_from_line(line):
+    return tuple([int(i) for i in line.strip().split()[0].split('x')])
+
+
+def _get_resolution_from_mirout():
+    mirout_stdout = subprocess.check_output(
+        ['mirout', _get_unity8_mir_socket()],
+        universal_newlines=True
+    ).split('\n')
+    grab_resolution = False
+    for line in mirout_stdout:
+        if grab_resolution:
+            return _grab_resolution_from_line(line)
+        if 'connected' in line:
+            grab_resolution = True
+    raise ValueError('Something for now')
+
+
+def _is_x_server_running():
+    if not os.environ.get('DISPLAY'):
+        return False
     try:
-        return _get_fbset_resolution()
-    except Exception:
-        return _get_hardcoded_resolution()
+        return bool(process_util.get_pids_for_process(PROCESS_NAME_X_SERVER))
+    except ValueError:
+        return False
+
+
+def _is_mir_based_server_running():
+    xdg_dir = os.environ.get(ENV_XDG_RUNTIME_DIR)
+    return xdg_dir is not None and os.path.exists(
+        os.path.join(xdg_dir, SOCKET_MIR)
+    )
+
+
+def query_current_display_server():
+    if _is_x_server_running():
+        return DISPLAY_SERVER_X
+    elif _is_mir_based_server_running():
+        return DISPLAY_SERVER_MIR
+    else:
+        raise RuntimeError('No or unknown display server running.')
 
 
 def _get_fbset_resolution():
@@ -44,24 +136,6 @@ def _get_fbset_resolution():
 
 def _get_fbset_output():
     return subprocess.check_output(["fbset", "-s", "-x"]).decode().strip()
-
-
-def _get_hardcoded_resolution():
-    name = image_codename()
-
-    resolutions = {
-        "generic": (480, 800),
-        "mako": (768, 1280),
-        "maguro": (720, 1280),
-        "manta": (2560, 1600),
-        "grouper": (800, 1280),
-    }
-
-    if name not in resolutions:
-        raise NotImplementedError(
-            'Device "{}" is not supported by Autopilot.'.format(name))
-
-    return resolutions[name]
 
 
 class Display(DisplayBase):
@@ -91,4 +165,4 @@ class Display(DisplayBase):
         :return: Tuple containing (x, y, width, height).
 
         """
-        return (0, 0, self._X, self._Y)
+        return 0, 0, self._X, self._Y
