79d9e91140eafeb93d5f9dc4bdc38f61ace84f7f
Omer Akram <om26er@ubuntu.com>
2016-11-23 19:12:47 +0500

    Fix build failure due to flake8 errors

--- autopilot/run.py
+++ autopilot/run.py
@@ -749,5 +749,6 @@ def main():
         print(e)
         exit(1)
 
+
 if __name__ == "__main__":
     main()
--- autopilot/tests/unit/introspection_base.py
+++ autopilot/tests/unit/introspection_base.py
@@ -38,6 +38,7 @@ class MockObject(Mock):
     def get_properties(self):
         return self.__dict__
 
+
 get_mock_object = MockObject
 
 
