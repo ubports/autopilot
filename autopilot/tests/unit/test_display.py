b5a7ffb3e02eb98304227a2a929aff406629619a
Santiago Baldassin <santiago.baldassin@canonical.com>
2017-03-09 17:01:08 -0300

    Using unittest.mock.patch

--- autopilot/tests/unit/test_display.py
+++ autopilot/tests/unit/test_display.py
@@ -17,10 +17,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 import unittest
+from unittest.mock import patch
 
 from autopilot.display import Display
-from mock import patch
-
 
 class DisplayTestCase(unittest.TestCase):
 
